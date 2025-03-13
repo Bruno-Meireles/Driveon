c2c576bfd6b4f44a32aa4c58e705a98a5b3dfd87
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2025-03-13 08:27:29 -0300

    working in Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,23 +1,23 @@
+import React from "react";
+import "./Hero.css";
 import banner from "../../assets/img/banner.png";
 
-const Hero = () => {
+const Hero: React.FC = () => {
   return (
-    <>
-      <section className="hero">
-        <div className="hero-text">
-          <h1>Start Your Workday Comfortably </h1>
-          <p>
-            Get In, sit black and experience the comfort of car that takes you
-            directly to your destination. Arriving work as a sardine with public
-            transport or sweating is a thing of the past.
-          </p>
-          <button>Discover </button>
-        </div>
-        <div>
-          <img src={banner} alt="" />
-        </div>
-      </section>
-    </>
+    <section className="hero-section">
+      <div className="hero-content">
+        <h1>Start Your Workday Comfortably</h1>
+        <p>
+          Get In, sit black and experience the comfort of car that takes you
+          directly to your destination. Arriving work as a sardine with public
+          transport or sweating is a thing of the past.
+        </p>
+        <button className="cta-button">Discover</button>
+      </div>
+      <div className="hero-image">
+        <img src={banner} alt="" />
+      </div>
+    </section>
   );
 };
 
