3e213ed61072d54dc781862df0a9ae5af9e0b42c
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2025-03-14 21:41:21 -0300

    work in Commitment

--- src/components/Commitment/Commitment.tsx
+++ src/components/Commitment/Commitment.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Commitment.css";
+import linha from "../../assets/img/linha.png";
 <img src={drivers} alt="Car img" />;
 import drivers from "../../assets/img/drivers.png";
 
@@ -7,13 +8,15 @@ const Commitment: React.FC = () => {
   return (
     <section className="commitment">
       <div className="commitment-content">
-        <h2>Our Commitment to Your Safety</h2>
+        <div className="commitment-container">
+        <img className="commitment-line" src={linha} alt="Line" />
+        <h2 className="commitment-title">Our Commitment to Your Safety</h2>
         <p>
           With every safetyfeature we add and every standard in the Community
           Guide that we uphold, we are committed to protecting you throughout
           the journey. Learn more about our Community Guide here..
         </p>
-        <span>See all safety features</span>
+        <span className="commitment-link">See all safety features</span></div>
       </div>
       <div className="commitment-image">
         <img src={drivers} alt="Car img" />
