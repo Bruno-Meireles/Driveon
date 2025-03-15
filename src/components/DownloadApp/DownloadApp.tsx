48a07404a9fb25d54b8fc5c2b2ba607a2a609d0a
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2025-03-14 22:45:26 -0300

    working in DownloadApp put images

--- src/components/DownloadApp/DownloadApp.tsx
+++ src/components/DownloadApp/DownloadApp.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./DownloadApp.css";
-import appImage from "../../assets/img/app.png";
 import googlePlay from "../../assets/img/google-play.webp";
 import playApp from "../../assets/img/playApp.png";
 import setas from "../../assets/img/setas.png";
@@ -8,28 +7,27 @@ import line from "../../assets/img/linha.png";
 
 const DownloadApp: React.FC = () => {
   return (
-    <section className="download-app">
-      <div className="download-content">
-        <div className="download-text">
-          <h2>Download the DriveOn application</h2>
-          <p>Get the best experience by using our mobile application.</p>
-          <div className="download-buttons">
-            <a href="#">
-              <img src={googlePlay} alt="Google Play Store" />
-            </a>
-            <a href="#">
-              <img src={playApp} alt="App Store" />
-            </a>
+    <>
+      <section className="download-app">
+        <div className="download-content">
+          <div className="download-text">
+            <h2>Download the DriveOn application to get more benefits and experience.</h2>
+            <div className="download-buttons">
+              <a href="#">
+                <img src={googlePlay} alt="Google Play Store" />
+              </a>
+              <a href="#">
+                <img src={playApp} alt="App Store" />
+              </a>
+            </div>
           </div>
         </div>
-        <div className="download-image">
-          <img src={appImage} alt="App preview" />
-        </div>
-      </div>
+      </section>
       <div className="register">
         <span>
           <h3>Register to be a driver partner </h3>
-          <img src={setas} alt="Arrow" /> <img src={line} alt="Line" />
+          <img src={setas} alt="Arrow" />
+          <img src={line} alt="Line" />
         </span>
         <span>
           <h3>Register to start your journey </h3>
@@ -37,7 +35,7 @@ const DownloadApp: React.FC = () => {
           <img src={setas} alt="Arrow" />
         </span>
       </div>
-    </section>
+    </>
   );
 };
 
